9990cb6212d49569fc09970d95da7f224a75d5cf
yate <geyate@gmail.com>
2019-10-29 19:25:02 +0800

    新添gaze交互模拟
    新增gaze交互模拟脚本
    新增 interactable 对象tag

---
+++ Assets/GazeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeController : MonoBehaviour
+{
+    public delegate void Hovered(bool isHoverd);
+    public Hovered hovered; 
+
+    GameObject[] interactableTargets;
+    // Start is called before the first frame update
+    private Camera arCamera;
+    void Start()
+    {
+        // 查找相机
+        arCamera = GameObject.FindObjectOfType<Camera>();
+        
+        interactableTargets = GameObject.FindGameObjectsWithTag("interactable");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Ray ray = new Ray (arCamera.transform.position, arCamera.transform.forward);//显示一条射线，只有在scene视图中才能看到
+       RaycastHit hit;
+    //    Debug.DrawLine(ray.origin,hit.point,Color.red,2);
+       if (Physics.Raycast (ray, out hit, 100)){
+           Debug.DrawLine(ray.origin,hit.point,Color.red,2);
+           var objectName = hit.collider.gameObject.name;
+            foreach(var a in interactableTargets){
+                if(a.name == objectName){
+                    a.GetComponent<InteractableTarget>().m_isHovered = true;
+                }else{
+                    a.GetComponent<InteractableTarget>().m_isHovered = false;
+                }
+            }          
+        //    if(hit.transform.tag=="interactable"){
+        //        var objectName = hit.collider.gameObject.name;
+        //        hovered(true);
+        //        Debug.Log("find " + objectName);
+        //    }
+       }else{
+           Debug.Log("no interactables");
+            foreach(var a in interactableTargets){
+                // hovered(false);
+                a.GetComponent<InteractableTarget>().m_isHovered = false;
+            }
+       }
+    }
+}
---
+++ Assets/InteractableTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InteractableTarget : MonoBehaviour
+{
+    private Renderer render;
+    public bool m_isHovered;
+    GazeController m_GazeController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_isHovered = false;
+        m_GazeController = GameObject.Find("GazeController").GetComponent<GazeController>();
+        // m_GazeController.hovered = IsHovered;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_isHovered){
+            GetComponent<Renderer>().material.color = Color.red;
+        }else{
+            GetComponent<Renderer>().material.color = Color.white;
+        }        
+    }
+
+    void IsHovered(bool isHoverd){
+        if(isHoverd){
+            GetComponent<Renderer>().material.color = Color.red;
+        }else{
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+        
+}
